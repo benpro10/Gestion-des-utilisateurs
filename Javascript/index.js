e664d3869cfb233cd7ded649913fce732bc1fca3
Exauce Benjamin Kyalondawa <exaucebenjamink@gmail.com>
2024-10-30 01:21:14 +0100

    ajout de gestion de la recherhe et de la promesse

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,14 +2,14 @@ const addNameUser = document.querySelector("#name");
 const addMailUser = document.querySelector("#email");
 const password = document.querySelector("#password");
 const btnAddUser = document.querySelector("#addUserButton");
-const searchMailName = document.querySelector("#searchEmailName");
+const searchMail = document.querySelector("#searchEmailName");
 const btnSearchUser = document.querySelector("#searchUserButton");
 const message = document.querySelector("#searchResult");
 const messagePwd1 = document.querySelector("#validationPasswordMessage1");
 const messagePwd2 = document.querySelector("#validationPasswordMessage2");
 const messagePwd3 = document.querySelector("#validationPasswordMessage3");
 const messagePwd4 = document.querySelector("#validationPasswordMessage4");
-const listUser = document.querySelector("#userList")
+const listUser = document.querySelector("#userList");
 
 // VALIDATION EMAIL PASSWORD NAME
 
@@ -30,7 +30,7 @@ function nombreCaractere(text) {
  */
 
 function compteEmailvalide(mail) {
-  let masque = /@gmail.com|@yahoo.com|@cloud.com/;
+  let masque = /@gmail.com|@yahoo.com|@icloud.com/;
   return masque.test(mail);
 }
 
@@ -90,26 +90,62 @@ function ErreurMessage(erreur) {
   message.textContent = erreur;
 }
 
-
-
 // AJOUT UTILISATEUR
 function ajouterUtilisateur(user) {
-      let userExisteDeja = false;
-      for (let i = 0; i < listUser.children.length; i++) {
-        if (listUser.children[i].textContent === user) {
-            userExisteDeja = true;
-          break;
-        }
-      }
-    
-      if (!userExisteDeja) {
-        let adduser = document.createElement("li");
-        listUser.appendChild(adduser);
-        adduser.textContent = user;
-      } else {
-        ErreurMessage("cet utilisateur existe déjà...");
-      }
+  let userExisteDeja = false;
+  for (let i = 0; i < listUser.children.length; i++) {
+    if (listUser.children[i].textContent === user) {
+      userExisteDeja = true;
+      break;
     }
+  }
+
+  if (!userExisteDeja) {
+    let adduser = document.createElement("li");
+    listUser.appendChild(adduser);
+    adduser.textContent = user;
+  } else {
+    ErreurMessage("cet utilisateur existe déjà...");
+  }
+}
+
+// RECHERCHER UTILISATEUR
+
+async function rechercherUserDansList(user) {
+  return new Promise((resolve) => {
+    // simuler une attente de 2 secondes avant de retourner le résultat
+    setTimeout(() => {
+      const masque = new RegExp(`\\b${user}\\b`, "i");
+      const userTrouve = Array.from(listUser.children).some(
+        (li) => masque.test(li.textContent)
+      );
+      resolve(userTrouve);
+    }, 3000);
+  });
+}
+
+async function gererLaRecherche() {
+  const userRechercher = searchMail.value.trim();
+  if (userRechercher === "") {
+    ErreurMessage("Veillez saisir l'utilisateur à rechercher");
+    return;
+  } else succussfullyMessage("Recherche en cours...");
+
+  try {
+    const userEstTrouve = await rechercherUserDansList(userRechercher);
+    if (userEstTrouve) {
+      succussfullyMessage(
+        `l'utilisateur ${userRechercher} a été trouvé dans la liste !!!`
+      );
+    } else {
+      ErreurMessage(
+        `l'utilisateur ${userRechercher} n'a pas été trouvé dans la liste !!!`
+      );
+    }
+  } catch (error) {
+    ErreurMessage("une erreur est survenue lors de la recherche");
+  }
+}
 
 btnAddUser.addEventListener("click", (e) => {
   e.preventDefault();
@@ -123,13 +159,15 @@ btnAddUser.addEventListener("click", (e) => {
       if (compteEmailvalide(addMailUser.value)) {
         if (passwordValide(password.value)) {
           succussfullyMessage("L'utilisateur a été ajouter avec succès");
-          ajouterUtilisateur(addMailUser.value)
+          ajouterUtilisateur(addMailUser.value);
         } else ErreurMessage("Veillez entrez un mot de passe valide");
       } else ErreurMessage("Veillez entrez un mail correct");
     } else
       ErreurMessage("Veillez entrez le nom sans chiffre ni caractère special");
   } else ErreurMessage("Ces champs doivent comporter plus de 4 Caractères");
 });
+
+btnSearchUser.addEventListener("click", gererLaRecherche);
 
 password.addEventListener("input", () => {
   colorValidationMessage(password.value);
